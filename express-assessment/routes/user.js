d667e03a56974b049a48791044a22f02da2331dc
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-03-27 17:45:40 -0700

    Updated the routes file

--- express-assessment/routes/user.js
+++ express-assessment/routes/user.js
@@ -1,7 +1,112 @@
 import express from "express";
+import prisma from "../db/index.js";
 
-const router = express.Router();
+export default function setupUserRouter() {
+  const router = express.Router();
 
-// Create the routes here
+  // Create the routes here
+  router.get("/", async function (request, response) {
+    const allUsers = await prisma.user.findMany({
+      where: {
+        userId: request.user.id,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-export default router;
+    response.status(200).json({
+      success: true,
+      users: allUsers,
+    });
+  });
+
+  router.post("/", async function (request, response) {
+    const newUsers = await prisma.user.create({
+      data: {
+        firstname: request.body.firstname,
+        lastname: request.body.lastname,
+        id: request.user.id,
+      },
+    });
+
+    console.log(newUsers);
+
+    response.status(201).json({
+      success: true,
+    });
+  });
+
+  router.put("/:userId", async function (request, response) {
+    const usersId = parseInt(request.params.userId);
+    try {
+      await prisma.users.update({
+        where: {
+          id: usersId,
+        },
+        data: {
+          ...request.body,
+        },
+      });
+
+      response.status(200).json({
+        success: true,
+      });
+    } catch (e) {
+      console.log(e);
+      if (e.code == "P2025") {
+        response.status(404).json({
+          success: false,
+        });
+      } else {
+        response.status(500).json({
+          success: false,
+        });
+      }
+    }
+  });
+
+  router.delete("/:userId", async function (request, response) {
+    const userId = parseInt(request.params.userId);
+    try {
+      await prisma.users.delete({
+        where: {
+          id: userId,
+        },
+      });
+
+      response.status(200).json({
+        success: true,
+      });
+    } catch (e) {
+      console.log(e);
+      if (e.code == "P2025") {
+        response.status(404).json({
+          success: false,
+        });
+      } else {
+        response.status(500).json({
+          success: false,
+        });
+      }
+    }
+  });
+
+  router.get("/:user", async function (request, response) {
+    const allUsers = await prisma.user.findMany({
+      where: {
+        userId: request.user.id,
+      },
+      include: {
+        user: false,
+      },
+    });
+
+    response.status(200).json({
+      success: false,
+      users: allUsers,
+    });
+  });
+
+  return router;
+}
--- express-assessment/server.js
+++ express-assessment/server.js
@@ -1,5 +1,7 @@
 import express from "express";
-import userRouter from "./routes/user";
+import userRouter from "./routes/user.js";
+
+
 
 export default function createServer() {
   const app = express();
