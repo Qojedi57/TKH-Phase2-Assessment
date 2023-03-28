d1e05745d746d84f74cb3b8b5246a655608da74f
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-03-27 17:24:48 -0700

    Updated displays the pokemon names

--- react-assessment/src/App.jsx
+++ react-assessment/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import useSWR, { SWRConfig } from "swr";
 
-function App() {
-  const [count, setCount] = useState(0)
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <SWRConfig value={{ fetcher }}>
+      <Pokemon />
+    </SWRConfig>
+  );
 
-export default App
+  function Pokemon() {
+    const url = " https://pokeapi.co/api/v2/pokemon";
+    const { data, error } = useSWR(url);
+
+    if (error) {
+      return <div>Error...</div>;
+    }
+    if (!data) {
+      return <div>Loading...</div>;
+    }
+
+    return <DisplayPokemon pokemon={data} />;
+  }
+
+  function DisplayPokemon({pokemon}) {
+  return <pre>{JSON.stringify(pokemon, null, 2)}</pre>;
+
+}
+}
+  
